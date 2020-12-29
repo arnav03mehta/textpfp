37bebc8edc6766d26074f5eda63ca76d4ac47014
/dev/Null <arnav2003mehta@gmail.com>
2020-12-30 00:00:08 +0530

    initial commit
    the python file that colors the text

---
+++ main.py
@@ -0,0 +1,63 @@
+import colorama
+from colorama import Fore, Back
+colorama.init(autoreset=True)
+i:str = Back.LIGHTBLACK_EX + Fore.YELLOW + """
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/+ossyyyyyyyysso+/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/oyyyhhhhhhhhhhhhhhyyyo/"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"+syhhhhhhhhhhhhhhhhhhhhhhys+"+Fore.YELLOW+""":::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/yhhhhhhhhhhhhhhhhhhhhhhhhhhhyhs"+Fore.YELLOW+""":::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/yhhhhhhhhhhhhhhhhhhhhhhhhhhhyhhho"+Fore.YELLOW+"""::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhho"+Fore.YELLOW+""":::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+"+Fore.YELLOW+"""::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::"""+Fore.RED+"shhhhhhhhhhhhhhhhhhhhhhhhhhhhhyhhhhhhy"+Fore.YELLOW+"""::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.RED+"+d"+Fore.BLACK+"m"+Fore.RED+"dhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"smmmmm"+Fore.RED+"dhhhhhhhhhhhhhhhhhhdd"+Fore.BLACK+"mmmmmm"+Fore.RED+"dhhhhhs"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"hNmmmmmmm"+Fore.RED+"hhhhhhhhhhhhhd"+Fore.BLACK+"mmNNNNNNmmm"+Fore.RED+"dhhhhs"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"mmmmddmNNm"+Fore.RED+"dhhhhhhhhhd"+Fore.BLACK+"mNNNNNNNNmmmmmm"+Fore.RED+"hhhy"+Fore.YELLOW+""":::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"/Nmmy"+Fore.WHITE+"-./"+Fore.BLACK+"hNNm"+Fore.RED+"dhhhhhhd"+Fore.BLACK+"mNNNNmdddmmmmmmmm"+Fore.RED+"dhhs"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"mmm"+Fore.WHITE+":   `/"+Fore.BLACK+"dNNm"+Fore.RED+"hhhh"+Fore.BLACK+"mNNmhs"+Fore.WHITE+"/:----+"+Fore.BLACK+"dNmmmmm"+Fore.RED+"hh+"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.BLACK+"hmm"+Fore.WHITE+":     `"+Fore.BLACK+"sNN"+Fore.RED+"dhhd"+Fore.BLACK+"NNh"+Fore.WHITE+"/.`       ."+Fore.BLACK+"NNmmmm"+Fore.RED+"hy/"+Fore.YELLOW+""":::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.RED+"o"+Fore.BLACK+"Nmh"+Fore.WHITE+"`     -"+Fore.BLACK+"NN"+Fore.RED+"dhhd"+Fore.BLACK+"NN"+Fore.WHITE+":`         ."+Fore.BLACK+"NNmmm"+Fore.RED+"hho"+Fore.YELLOW+"""::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::"""+Fore.RED+"/d"+Fore.BLACK+"NNh"+Fore.WHITE+":`  ."+Fore.BLACK+"yNm"+Fore.RED+"hhhh"+Fore.BLACK+"mNh"+Fore.WHITE+"+..../-../"+Fore.BLACK+"hmmm"+Fore.RED+"dhhy"+Fore.YELLOW+""":::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::"""+Fore.RED+"od"+Fore.BLACK+"mNmhyhdNm"+Fore.RED+"hhhhhh"+Fore.BLACK+"mNmmmmmmddmmmm"+Fore.RED+"dhhhy/"+Fore.YELLOW+""":::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::"""+Fore.RED+"yhd"+Fore.BLACK+"mNNNm"+Fore.RED+"dhhhdhhhhd"+Fore.BLACK+"mNNNNNNNm"+Fore.RED+"ddhhhhy+"+Fore.YELLOW+"""::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/syhddhhhhhhhhhhhhhdd"+Fore.BLACK+"mmm"+Fore.RED+"dhhhhhhhy/"+Fore.YELLOW+""":::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/oyhhhhhhhhhhhhhhhhhhhhhyhhhyyo/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/oyyyhhhhhhhhhhhhhhhhhyyhdd/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/+oyhhhhhhhhdddmmdoyNNd/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"odddddddmNNNNNmmdmNy/"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"oNhhhhhhhNNNNNmmmmmm+"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"mNhddhhdmNNNNNNmmmmd+"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"+ddhdmhhmmNNNNNmmmNNm"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/hmhmhmhhdmNNNNNNdmNNm+"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"hdhhdmhhhNNNNNNNNdhhhho"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"+mNddhmhhhNNNNNNNNNhhhho"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"+mmmhdhmhhdNNNNNNNNNNmmmh/"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"smNmmhhhhhhdNNNNNNNNNNmsNNd+"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::
+:::::::::::::::::"""+Fore.WHITE+"-....------------------"+Fore.RED+"hNNmdhydhdhdmmNmmmmNmmmhhmNm+"+Fore.YELLOW+""":::::::::::::::::::::::::::::::
+:::::::::::::::::::"""+Fore.WHITE+"-``    ````...---"+Fore.YELLOW+"::::"+Fore.RED+"ohhdhdydhddddmmddddmdddmy/dNd"+Fore.YELLOW+""":::::::::::::::::::::::::::::::
+::::::::::::::::::::::"""+Fore.WHITE+"-----"+Fore.YELLOW+"::::::::::::::::"""+Fore.RED+"//ohhhhdddmNmddmNmdds+:/o/"+Fore.YELLOW+""":::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"shhhhhhdNNNNNNNNd"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/yhhdhhhmNNNNNNNs"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/hmmNmNNNNNNNNN+"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"/+mmNNNysmNdddd+"+Fore.YELLOW+""":::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::"""+Fore.RED+"//+shsyhddNNNNNy/oNmmmmh/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::"""+Fore.RED+"//+osssyyyhmmNdhdNNNNNNNmhhmmmmmmdysoo+/"+Fore.YELLOW+"""::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::"""+Fore.RED+"/syhhhhhhhhhhmmmmmNNNNNmmmmhmmNmmNNmNhhhhhyo"+Fore.YELLOW+"""::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::"""+Fore.RED+"+syyyyhhhhhhhhhhhhhhhhhhhhdmmNNNNNNmhyyyso/"+Fore.YELLOW+"""::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::"""+Fore.RED+"//++ooosssssyyyyyyyyyyssssssso+++//"+Fore.YELLOW+"""::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+"""
+
+print(i)
